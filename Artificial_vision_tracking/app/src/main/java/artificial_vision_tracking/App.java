f84f4c88dcf6a3e95c36dd6a6d2671786f102a6d
Lorenzo Bacchini <lorenzo.bacchini4@studio.unibo.it>
2024-06-29 17:30:41 +0200

    now test application recognize simple qr code

--- Artificial_vision_tracking/app/src/main/java/artificial_vision_tracking/App.java
+++ Artificial_vision_tracking/app/src/main/java/artificial_vision_tracking/App.java
@@ -3,12 +3,53 @@
  */
 package artificial_vision_tracking;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Rect;
+import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.global.opencv_imgproc;
+import org.bytedeco.opencv.opencv_aruco.Board;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.opencv.opencv_objdetect.QRCodeDetector;
+import org.bytedeco.javacpp.BytePointer;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+public class App {
+    public static void main(String[] args) throws FrameGrabber.Exception, InterruptedException {
+        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+        grabber.start();
+        Board board = new Board();
+        CanvasFrame canvas = new CanvasFrame("Webcam");
+        canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        QRCodeDetector qrCodeDetector = new QRCodeDetector();
+        OpenCVFrameConverter.ToMat converterToMat = new OpenCVFrameConverter.ToMat();
+
+        while (canvas.isVisible() && (grabber.grab()) != null) {
+            Frame frame = grabber.grab();
+
+
+            Mat mat = converterToMat.convert(frame);
+          
+            Mat points = new Mat();
+            BytePointer bytePointer = qrCodeDetector.detectAndDecode(mat); 
+            //String decodedText = qrCodeDetector.detectAndDecode(mat).getString();
+          
+            if (bytePointer!=null && !bytePointer.toString().isEmpty()) {
+                String decodedText = bytePointer.getString();
+                System.out.println("QR Code detected: " + decodedText);
+                for (int i = 0; i < points.size().height(); i++) {
+                    Rect rect = new Rect(points.ptr(i));
+                    opencv_imgproc.rectangle(mat, rect, new Scalar(0, 255, 0, 0));
+                }
+            }
+          
+            canvas.showImage(grabber.grab());
+        }
+        grabber.stop();
+        grabber.close();
+        canvas.dispose();
     }
 }
