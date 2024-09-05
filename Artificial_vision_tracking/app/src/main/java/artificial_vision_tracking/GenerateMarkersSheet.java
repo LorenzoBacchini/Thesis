e752a92afb4f798c6ca1272d1bbd4acfc6bf9e65
Lorenzo Bacchini <lorenzo.bacchini4@studio.unibo.it>
2024-09-05 12:34:32 +0200

    refactor of generateMarkerSheet

--- Artificial_vision_tracking/app/src/main/java/artificial_vision_tracking/GenerateMarkersSheet.java
+++ Artificial_vision_tracking/app/src/main/java/artificial_vision_tracking/GenerateMarkersSheet.java
@@ -2,7 +2,6 @@ package artificial_vision_tracking;
 
 import org.opencv.core.CvType;
 import org.opencv.objdetect.Dictionary;
-import org.opencv.objdetect.Objdetect;
 import org.opencv.objdetect.GridBoard;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
@@ -10,31 +9,52 @@ import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 
 public class GenerateMarkersSheet {
-    public void generateMarkersSheet() {
-        int markersX = 8; // Numero di marker in orizzontale
-        int markersY = 10; // Numero di marker in verticale
-        float markerLength = 50; // Lunghezza del marker in pixel
-        float markerSeparation = 10; // Separazione tra i marker in pixel
+    private static final int FULL_COLOR = 255;
+    private static final int IMAGE_MARGIN_SIZE = 10;
+    private static final int MARKER_BORDER_BITS= 1;
 
-        // Crea il dizionario Aruco per i marker 4x4
-        Dictionary dictionary = Objdetect.getPredefinedDictionary(Objdetect.DICT_4X4_100);
-        // Crea una griglia di marker Aruco
+    // Horizontal markers
+    private int markersX;
+    // Vertical markers
+    private int markersY;
+    // Marker length in pixels
+    private float markerLength;
+    // Separation between markers in pixels
+    private float markerSeparation;
+    // Aruco dictionary
+    Dictionary dictionary;
+    // Output file name
+    private String fileName;
+
+    public GenerateMarkersSheet(int markersX, int markersY, float markerLength, float markerSeparation, Dictionary dictionary, String fileName) {
+        this.markersX = markersX;
+        this.markersY = markersY;
+        this.markerLength = markerLength;
+        this.markerSeparation = markerSeparation;
+        this.dictionary = dictionary;
+        this.fileName = fileName;
+    }
+
+    /**
+     * Method to generate the Aruco markers sheet
+     */
+    public void generateMarkersSheet() {
         GridBoard gridBoard = new GridBoard(new Size(markersX, markersY), markerLength, markerSeparation, dictionary);
 
-        // Calcola la dimensione totale dell'immagine
+        // Calculate the total width and height of the image
         int totalWidth = (int) (markersX * (markerLength + markerSeparation) - markerSeparation);
         int totalHeight = (int) (markersY * (markerLength + markerSeparation) - markerSeparation);
         Size imageSize = new Size(totalWidth, totalHeight);
 
-        // Crea un'immagine vuota
-        Mat markerImage = new Mat(imageSize, CvType.CV_8UC1, new Scalar(255));
+        // Create an image with a white background
+        Mat markerImage = new Mat(imageSize, CvType.CV_8UC1, new Scalar(FULL_COLOR));
 
-        // Disegna la griglia dei marker sull'immagine
-        gridBoard.generateImage(imageSize, markerImage, 10, 1);
+        // Drawing the markers grid
+        gridBoard.generateImage(imageSize, markerImage, IMAGE_MARGIN_SIZE, MARKER_BORDER_BITS);
 
-        // Salva l'immagine con i marker
-        Imgcodecs.imwrite("aruco_markers.png", markerImage);
+        // Saving the image
+        Imgcodecs.imwrite(fileName + ".png", markerImage);
 
-        System.out.println("Immagine dei marker Aruco generata e salvata come aruco_markers.png");
+        System.out.println("Immagine dei marker Aruco generata e salvata come " + fileName + ".png");
     }
 }
